6c647d0585af5a6239816e0cc6f82570fa7aaf91
Sergio Garguir <sergiogarguir@gmail.com>
2023-10-16 11:57:26 -0300

    Fin seccion 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public heroNamesRemoved: string[] = [];
+
+  removeLastHero():void {
+    const deletedHero = this.heroNames.pop();
+    if(deletedHero !== undefined){
+      this.heroNamesRemoved.push(deletedHero);
+    }
+  }
+}
